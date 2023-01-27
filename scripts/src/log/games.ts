b8876afb87118354869cb8e4433483795fee05c3
Tim Hårek Andreassen <tim@harek.no>
2023-01-27 23:32:00 +0100

    chore(scripts): Use dynamic prompts
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- scripts/src/log/games.ts
+++ scripts/src/log/games.ts
@@ -1,34 +1,42 @@
-// @deno-types="./mod.d.ts"
+// @deno-types="../../mod.d.ts"
 
-import { Input, Number } from '../../deps.ts';
+import { Input, Number, prompt } from '../../deps.ts';
 import { getEntryDate } from '../util.ts';
 
 export async function logGame(type: 'game') {
   const currentDate = new Date().toISOString().split('T')[0];
 
-  const title: string = await Input.prompt('What did you play?');
-  const platform: string = await Input.prompt(
-    'Which platform did you play on?',
-  );
-  const releaseYear: number = await Number.prompt(
-    'Which year did the game release?',
-  );
-  const date: string = await Input.prompt(
-    {
-      message: 'When did you play it? (YYYY-MM-DD)',
-      suggestions: [currentDate],
-    },
-  ) ?? currentDate;
-  const rating: number = await Number.prompt({
-    message: `How many stars for ${title}? (1-5)`,
+  const result = await prompt([{
+    name: 'title',
+    message: 'What did you play?',
+    type: Input,
+  }, {
+    name: 'platform',
+    message: 'Which platform did you play it on?',
+    type: Input,
+  }, {
+    name: 'releaseYear',
+    message: 'Which year did the game release?',
+    type: Number,
+  }, {
+    name: 'date',
+    message: 'When did you play it? (YYYY-MM-DD)',
+    type: Input,
+    suggestions: [currentDate],
+  }, {
+    name: 'rating',
+    message: 'How many stars? (1-5)',
+    type: Number,
     min: 1,
     max: 5,
-  });
+  }]);
+
+  const { title, date, releaseYear, rating, platform } = result;
 
   const gameEntry: Log.IGameEntry = {
     title: title,
     type: type,
-    date: [getEntryDate(date)]],
+    date: [getEntryDate(date)],
     details: {
       release_year: releaseYear,
       my_rating: rating,
