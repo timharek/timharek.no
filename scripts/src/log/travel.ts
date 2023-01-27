9e8a3a1412c5674d58fad1769eac5820ea10a523
Tim Hårek Andreassen <tim@harek.no>
2023-01-27 23:51:47 +0100

    refactor(scripts): Use dynamic prompt
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- scripts/src/log/travel.ts
+++ scripts/src/log/travel.ts
@@ -1,42 +1,57 @@
 // @deno-types="./mod.d.ts"
 
-import { Input, Select } from '../../deps.ts';
+import { Input, prompt, Select } from '../../deps.ts';
 import { getCurrentDate, getEntryDate } from '../util.ts';
 
 export async function logTrip(type: 'travel') {
   const currentDate = getCurrentDate();
 
-  const country: string = await Input.prompt(
-    'Which country did you travel to?',
-  );
-  const countryEmoji: string = await Input.prompt(
-    `What is the flag-emoji for ${country}`,
-  );
-  const cities: string = await Input.prompt(
-    'Which cities did you visit? (comma seperated values)',
-  );
-  const departure: string = await Input.prompt(
+  const result = await prompt([
     {
+      name: 'country',
+      message: 'Which country did you travel to?',
+      type: Input,
+    },
+    {
+      name: 'countryEmoji',
+      message: 'What is the flag-emoji?',
+      type: Input,
+    },
+    {
+      name: 'cities',
+      message: 'Which cities did you visit? (comma seperated values)',
+      type: Input,
+    },
+    {
+      name: 'departure',
       message: 'When did you departure? (YYYY-MM-DD)',
+      type: Input,
       suggestions: [currentDate],
     },
-  ) ?? currentDate;
-  const arrival: string = await Input.prompt(
     {
+      name: 'arrival',
       message: 'When did you arrive home? (YYYY-MM-DD)',
+      type: Input,
       suggestions: [currentDate],
     },
-  ) ?? currentDate;
-  const occasion: 'business' | 'pleasure' = await Select.prompt({
-    message: 'What was the occasion?',
-    options: [
-      { name: 'Business', value: 'business' },
-      { name: 'Pleasure', value: 'pleasure' },
-    ],
-  });
+    {
+      name: 'occasion',
+      message: 'What was the occasion?',
+      type: Select,
+      options: [
+        { name: 'Business', value: 'business' },
+        { name: 'Pleasure', value: 'pleasure' },
+      ],
+    },
+    {
+      name: 'title',
+      message: 'What would you call your trip',
+      type: Input,
+    },
+  ]);
 
-  const title: string = await Input.prompt(`What would you call your trip`);
-
+  const { title, departure, arrival, occasion, country, countryEmoji, cities } =
+    result;
   const travelEntry: Log.ITravelEntry = {
     title: title,
     type: type,
