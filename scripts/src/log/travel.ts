a05f7d8f577e1926552906b61e4c916630ab65c4
Tim Hårek Andreassen <tim@harek.no>
2023-01-28 17:00:04 +0100

    chore(scripts): Use List for prompt with multiple values
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- scripts/src/log/travel.ts
+++ scripts/src/log/travel.ts
@@ -1,57 +1,56 @@
-// @deno-types="./mod.d.ts"
+// @deno-types="../../mod.d.ts"
 
-import { Input, prompt, Select } from '../../deps.ts';
+import { Input, List, prompt, Select } from '../../deps.ts';
 import { getCurrentDate, getEntryDate } from '../util.ts';
 
 export async function logTrip(type: 'travel') {
   const currentDate = getCurrentDate();
 
-  const result = await prompt([
-    {
-      name: 'country',
-      message: 'Which country did you travel to?',
-      type: Input,
-    },
-    {
-      name: 'countryEmoji',
-      message: 'What is the flag-emoji?',
-      type: Input,
-    },
-    {
-      name: 'cities',
-      message: 'Which cities did you visit? (comma seperated values)',
-      type: Input,
-    },
-    {
-      name: 'departure',
-      message: 'When did you departure? (YYYY-MM-DD)',
-      type: Input,
-      suggestions: [currentDate],
-    },
-    {
-      name: 'arrival',
-      message: 'When did you arrive home? (YYYY-MM-DD)',
-      type: Input,
-      suggestions: [currentDate],
-    },
-    {
-      name: 'occasion',
-      message: 'What was the occasion?',
-      type: Select,
-      options: [
-        { name: 'Business', value: 'business' },
-        { name: 'Pleasure', value: 'pleasure' },
-      ],
-    },
-    {
-      name: 'title',
-      message: 'What would you call your trip',
-      type: Input,
-    },
-  ]);
-
   const { title, departure, arrival, occasion, country, countryEmoji, cities } =
-    result;
+    await prompt([
+      {
+        name: 'country',
+        message: 'Which country did you travel to?',
+        type: Input,
+      },
+      {
+        name: 'countryEmoji',
+        message: 'What is the flag-emoji?',
+        type: Input,
+      },
+      {
+        name: 'cities',
+        message: 'Which cities did you visit? (comma separated)',
+        type: List,
+      },
+      {
+        name: 'departure',
+        message: 'When did you departure? (YYYY-MM-DD)',
+        type: Input,
+        suggestions: [currentDate],
+      },
+      {
+        name: 'arrival',
+        message: 'When did you arrive home? (YYYY-MM-DD)',
+        type: Input,
+        suggestions: [currentDate],
+      },
+      {
+        name: 'occasion',
+        message: 'What was the occasion?',
+        type: Select,
+        options: [
+          { name: 'Business', value: 'business' },
+          { name: 'Pleasure', value: 'pleasure' },
+        ],
+      },
+      {
+        name: 'title',
+        message: 'What would you call your trip',
+        type: Input,
+      },
+    ]);
+
   const travelEntry: Log.ITravelEntry = {
     title: title,
     type: type,
@@ -60,16 +59,10 @@ export async function logTrip(type: 'travel') {
       occasion: occasion,
       location: {
         country: { name: country, emoji: countryEmoji },
-        cities: getCities(cities),
+        cities: cities,
       },
     },
   };
 
   return travelEntry;
-}
-
-function getCities(cities: string) {
-  const citiesNoWhitespace = cities.replace(' ', '');
-  const citiesArray = citiesNoWhitespace.split(',');
-  return citiesArray;
 }
