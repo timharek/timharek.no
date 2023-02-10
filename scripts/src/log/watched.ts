7425a256e512c2a23055933d19020538fe3bfc5e
Tim Hårek Andreassen <tim@harek.no>
2023-02-10 18:04:06 +0100

    chore(script): Add season prompt for TV Shows
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- scripts/src/log/watched.ts
+++ scripts/src/log/watched.ts
@@ -3,14 +3,26 @@
 import { getMovie, Input, Number, OMDB, prompt } from '../../deps.ts';
 import { getCurrentDate, getEntryDate } from '../util.ts';
 
-export async function logMovieOrTv(type: 'movie' | 'tv') {
+export async function logMovieOrTv(logType: 'movie' | 'tv') {
   const currentDate = getCurrentDate();
 
-  const { title, date, rating } = await prompt([
+  const { title, date, rating, season } = await prompt([
     {
       name: 'title',
       message: 'What did you watch?',
       type: Input,
+    },
+    {
+      name: 'season',
+      message: 'Which season?',
+      type: Input,
+      before: async (_, next) => {
+        if (logType === 'tv') {
+          await next();
+        } else {
+          await next('date');
+        }
+      },
     },
     {
       name: 'date',
@@ -36,14 +48,14 @@ export async function logMovieOrTv(type: 'movie' | 'tv') {
   const entry: OMDB = await getMovie(options);
 
   const watchedEntry: Log.IWatchedEntry = {
-    title: entry.title,
-    type: type,
+    title: `${entry.title}${season ? ` S${season}` : ''}`,
+    type: logType,
     date: [getEntryDate(date)],
     details: {
       release_year: entry.year,
       my_rating: rating,
       genres: entry.genre,
-      ...(type === 'movie' && { director: entry.director }),
+      ...(logType === 'movie' && { director: entry.director }),
     },
   };
 
