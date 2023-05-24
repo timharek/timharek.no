21d1b85718d0d932f9dc48ca5d48685804f20522
Tim Hårek Andreassen <tim@harek.no>
2023-05-24 22:02:58 +0200

    chore(fresh): Add _app
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Tim Hårek</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
         <img
