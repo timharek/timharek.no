af8386234df52b8a369379a9cb44c699355c7d1e
Tim Hårek Andreassen <tim@harek.no>
2023-05-24 23:21:28 +0200

    feat(fresh): Add initial blog-post page
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,8 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $3 from "./routes/blog/[slug].tsx";
+import * as $4 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -14,7 +15,8 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/_app.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/blog/[slug].tsx": $3,
+    "./routes/index.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/blog/[slug].tsx
@@ -0,0 +1,51 @@
+import { Head } from "$fresh/runtime.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { render } from "https://deno.land/x/gfm@0.1.26/mod.ts";
+import { extract } from "https://deno.land/std@0.188.0/front_matter/any.ts";
+
+interface BlogPostProps {
+  markdown: string;
+  frontMatter: {
+    title: string;
+  };
+}
+
+export const handler: Handlers = {
+  async GET(_req, ctx) {
+    const slug = ctx.params.slug;
+    if (slug === "tim") {
+      return new Response("", {
+        status: 307,
+        headers: { location: "/hallo" },
+      });
+    }
+
+    const url = new URL(`../../content/blog/${slug}.md`, import.meta.url);
+    const fileContent = await Deno.readTextFile(url);
+    const { body, attrs } = extract(fileContent);
+    const page: BlogPostProps = {
+      markdown: body,
+      frontMatter: attrs as BlogPostProps["frontMatter"] ?? {},
+    };
+    const resp = ctx.render(page);
+    return resp;
+  },
+};
+
+export default function BlogPost({ data }: PageProps<BlogPostProps>) {
+  const { markdown, frontMatter } = data;
+  const body = render(markdown);
+  const title = frontMatter.title;
+
+  return (
+    <>
+      <Head>
+        <title>{title} - Blog - Tim Hårek</title>
+      </Head>
+      <article class="max-w-screen-md mx-auto px-4">
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: body }}></div>
+      </article>
+    </>
+  );
+}
