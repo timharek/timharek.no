a15f5d6950e6cccd61505dce3d13fa59ff5e82b4
Tim Hårek Andreassen <tim@harek.no>
2023-05-27 12:59:30 +0200

    feat(fresh): Load pages with dynamic routes
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,7 +3,7 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[...slug].tsx";
+import * as $0 from "./routes/[...page].tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/blog/[slug].tsx";
@@ -13,7 +13,7 @@ import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[...slug].tsx": $0,
+    "./routes/[...page].tsx": $0,
     "./routes/_app.tsx": $1,
     "./routes/api/joke.ts": $2,
     "./routes/blog/[slug].tsx": $3,
---
+++ routes/[...page].tsx
@@ -0,0 +1,59 @@
+import { Head } from "$fresh/runtime.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { render } from "https://deno.land/x/gfm@0.1.26/mod.ts";
+import { extract } from "https://deno.land/std@0.188.0/front_matter/any.ts";
+import { exists } from "https://deno.land/std@0.189.0/fs/exists.ts";
+
+interface Props {
+  markdown: string;
+  frontMatter: {
+    title: string;
+  };
+}
+
+export const handler: Handlers = {
+  async GET(_req, ctx) {
+    const page = ctx.params.page;
+
+    const commonPath = `../content/${page}`;
+    let pagePath = new URL(commonPath, import.meta.url);
+    const dirOrFile = await exists(pagePath);
+    if (!dirOrFile) {
+      pagePath = new URL(`${commonPath}.md`, import.meta.url);
+    } else {
+      pagePath = new URL(`${commonPath}/index.md`, import.meta.url);
+    }
+
+    try {
+      const fileContent = await Deno.readTextFile(pagePath);
+      const { body, attrs } = extract(fileContent);
+      const page: Props = {
+        markdown: body,
+        frontMatter: attrs as Props["frontMatter"] ?? {},
+      };
+      const resp = ctx.render(page);
+      return resp;
+    } catch (error) {
+      console.error(error);
+      return ctx.renderNotFound();
+    }
+  },
+};
+
+export default function BlogPost({ data }: PageProps<Props>) {
+  const { markdown, frontMatter } = data;
+  const body = render(markdown);
+  const title = frontMatter.title;
+
+  return (
+    <>
+      <Head>
+        <title>{title} - Tim Hårek</title>
+      </Head>
+      <article class="max-w-screen-md mx-auto px-4">
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: body }}></div>
+      </article>
+    </>
+  );
+}
