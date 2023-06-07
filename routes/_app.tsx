48c5e53ceaec8f25a06c9874015bd06cd0b7381f
Tim Hårek Andreassen <tim@harek.no>
2023-06-07 20:05:17 +0200

    chore(fresh): Remove stuff
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,27 +12,6 @@ export default function App({ Component }: AppProps) {
           name="description"
           content={config.description}
         />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#FCD34D" />
       </Head>
       <body class="bg-zinc-900 text-white">
         <Header />
