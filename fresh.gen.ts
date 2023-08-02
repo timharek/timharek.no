f9a9585649db569f44712eb07e58c86dce65ccc9
Tim Hårek Andreassen <tim@harek.no>
2023-08-02 21:03:53 +0200

    chore(fresh): Update
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[...page].tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/about/cv.tsx";
@@ -28,7 +27,6 @@ const manifest = {
     "./islands/Counter.tsx": $$0,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
