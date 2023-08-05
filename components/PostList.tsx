c0d7f58399c6761f6f4c737daeaf5988054c790e
Tim Hårek Andreassen <tim@harek.no>
2023-08-05 17:54:35 +0200

    chore(fresh): Improve styling for PostList
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -6,8 +6,11 @@ export function PostList({ posts }: Props) {
   return (
     <ul class="space-y-4">
       {posts.map((post) => (
-        <li class="flex gap-2">
-          <a class="text-primary hover:underline" href={post.path}>
+        <li class="border-l-4 border-primary pl-2 flex flex-wrap-reverse gap-2 justify-between w-full">
+          <a
+            class="text-primary hover:underline max-md:w-full"
+            href={post.path}
+          >
             {post.title}
           </a>
           {post.date && (
