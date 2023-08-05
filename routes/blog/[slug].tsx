ee896d551877e65ec978a9d72925efd0908d0989
Tim Hårek Andreassen <tim@harek.no>
2023-08-05 18:32:33 +0200

    feat(fresh): Add initial Metadata styling
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- routes/blog/[slug].tsx
+++ routes/blog/[slug].tsx
@@ -4,6 +4,7 @@ import { render } from "gfm/mod.ts";
 import { css, getBlogPost, slugify } from "../../src/utils.ts";
 import { ServerState } from "../_middleware.ts";
 import { PageHeader } from "../../components/PageHeader.tsx";
+import { config } from "../../config.ts";
 
 interface BlogPostProps {
   post: Post;
@@ -60,17 +61,46 @@ export default function BlogPost({ data }: PageProps<BlogPostProps>) {
         <PageHeader title={title} date={post.date} updated={post.updated} />
         <div class="markdown-body" dangerouslySetInnerHTML={{ __html: body }}>
         </div>
-        <ul>
-          {post.taxonomies &&
-            post.taxonomies.tags.map((tag) => (
-              <li>
-                <a href={`/tags/${slugify(tag)}`}>
-                  {tag}
-                </a>
-              </li>
-            ))}
-        </ul>
+        <Metadata tags={post.taxonomies?.tags} postTitle={title} />
       </article>
     </>
+  );
+}
+
+interface MetadataProps {
+  tags?: string[];
+  postTitle: string;
+}
+
+function Metadata({ tags, postTitle }: MetadataProps) {
+  return (
+    <div class="border-y-2 border-primary py-4 mt-4 flex flex-wrap gap-4 justify-between items-center">
+      <div class="">
+        <div class="flex gap-2">
+          <h2>Tagged with</h2>
+          {tags && (
+            <ul class="flex gap-1">
+              {tags.sort((a, b) => a.localeCompare(b)).map((tag) => (
+                <li>
+                  <a
+                    class="text-primary hover:underline"
+                    href={`/tags/${slugify(tag)}`}
+                  >
+                    {tag}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div class="">(count) words</div>
+      </div>
+      <a
+        class="border border-primary px-3 py-2 rounded-lg text-(primary hover:black) bg-(hover:primary) transition-all duration-150"
+        href={`mailto:${config.email}?subject=RE: ${postTitle}`}
+      >
+        Reply via email
+      </a>
+    </div>
   );
 }
