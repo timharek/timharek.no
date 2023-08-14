cef69e6b548168f98a12155314b0decc58947c52
Tim Hårek Andreassen <tim@harek.no>
2023-08-14 20:34:48 +0200

    fix(rss): Escape HTML
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- routes/rss.xml/index.tsx
+++ routes/rss.xml/index.tsx
@@ -5,6 +5,7 @@ import { getAllBlogPosts } from "../../src/markdown.ts";
 import { ServerState } from "../_middleware.ts";
 import { render } from "gfm/mod.ts";
 export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.8.1?target=esnext";
+import { escapeHtml } from "https://deno.land/x/escape_html@1.0.0/mod.ts";
 
 interface RSSProps {
   posts: Post[];
@@ -27,30 +28,31 @@ function generateRssFeed(posts: Post[]): string {
       return `<category term="${sanitizeHtml(tag)}" />`;
     }).filter((tag) => tag !== undefined);
 
-    return `<entry xml:lang="en">
-		<title>${sanitizeHtml(post.title)}</title>
-		<author>
-			<name>${config.author}</name>
-			<email>${config.email}</email>
-			<uri>${config.base_url}</uri>
-			</author>
-		<published>${post.date.toISOString()}</published>
-		<updated>${
-      post.updated ? post.updated.toISOString() : post.date.toISOString()
-    }</updated>
-		<link rel="alternate" href="${config.base_url}/${post.path}" type="text/html"/>
-		<id>${config.base_url}/${post.path}</id>
-    ${tags ? tags.join("\n") : ""}
-		<content type="html">
-      ${render(post.content)}
-      ${
+    const content = escapeHtml(`${render(post.content).toString()}
+${
       sanitizeHtml(
         `<a href="mailto:${config.email}?subject=${post.title}">Reply via e-mail</a>`,
-      )
-    }
-    </content>
-	</entry>
-`;
+      ).toString()
+    }`);
+    console.log(content);
+    return `<entry xml:lang="en">
+	<title>${sanitizeHtml(post.title)}</title>
+	<author>
+		<name>${config.author}</name>
+		<email>${config.email}</email>
+		<uri>${config.base_url}</uri>
+		</author>
+	<published>${post.date.toISOString()}</published>
+	<updated>${
+      post.updated ? post.updated.toISOString() : post.date.toISOString()
+    }</updated>
+	<link rel="alternate" href="${config.base_url}${post.path}" type="text/html"/>
+	<id>${config.base_url}/${post.path}</id>
+  ${tags ? tags.join("\n") : ""}
+  <content type="html">
+    ${content}
+  </content>
+</entry>`;
   });
 
   const feedUrl = `${config.base_url}/rss.xml`;
