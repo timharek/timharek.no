7fa9dde7b8749cca8f36c6b891d7a1cd8e412040
Tim Hårek Andreassen <tim@harek.no>
2023-08-14 18:24:53 +0200

    fix(breadcrumbs): Weird spacing
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -11,7 +11,7 @@ interface BreadcrumbsProps {
 export function Breadcrumbs({ crumbs }: BreadcrumbsProps) {
   return (
     <nav class="max-w-screen-md mx-auto px-4 mb-4">
-      <ol class="flex gap-2">
+      <ol class="flex gap-2 flex-wrap">
         {crumbs.map((crumb) => {
           if (crumb.current) {
             return (
@@ -21,7 +21,7 @@ export function Breadcrumbs({ crumbs }: BreadcrumbsProps) {
             );
           }
           return (
-            <li class="after:content-['/'] after:pl-1">
+            <li class="after:content-['/'] after:pl-1 flex gap-1">
               <a class="text-primary hover:underline" href={crumb.path}>
                 {crumb.title}
               </a>
