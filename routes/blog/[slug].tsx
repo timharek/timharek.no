6721e591924795521bc2fe58660d164a79eafec2
Tim Hårek Andreassen <tim@harek.no>
2023-08-17 14:37:32 +0200

    chore(markdown): Add systemd syntax
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- routes/blog/[slug].tsx
+++ routes/blog/[slug].tsx
@@ -13,6 +13,7 @@ import { slugify } from "../../src/utils.ts";
 import "https://esm.sh/prismjs@1.29.0/components/prism-typescript?no-check";
 import "https://esm.sh/prismjs@1.29.0/components/prism-bash?no-check";
 import "https://esm.sh/prismjs@1.29.0/components/prism-json?no-check";
+import "https://esm.sh/prismjs@1.29.0/components/prism-systemd?no-check";
 
 interface BlogPostProps {
   post: Post;
