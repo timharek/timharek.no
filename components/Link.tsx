753c637a0ef42cf3f5a5eea64be252c8c49ea247
Tim Hårek Andreassen <tim@harek.no>
2023-08-18 23:55:45 +0200

    fix(header): Exclude label from header nav
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- components/Link.tsx
+++ components/Link.tsx
@@ -4,9 +4,13 @@ interface LinkProps extends JSX.HTMLAttributes<HTMLAnchorElement> {
   label: string;
 }
 export function Link(props: LinkProps) {
+  const { label, ...propsWithoutLabel } = props;
   return (
-    <a {...props} class="text-primary hover:underline font-semibold">
-      {props.label}
+    <a
+      {...propsWithoutLabel}
+      class="text-primary hover:underline font-semibold"
+    >
+      {label}
     </a>
   );
 }
