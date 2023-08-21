2d4e31e8fb4ec71260263f853c258ecc2010740e
Tim Hårek Andreassen <tim@harek.no>
2023-08-21 13:23:36 +0200

    feat: Add counter to tags
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- content/todo.md
+++ content/todo.md
@@ -10,7 +10,6 @@ updated = 2023-08-19
 - Add sitemap (not xml)
 - Add automatic share cover-image
 - Add query params for CV, Work, Logs++
-- Add counter to tags
 - Add /now page
 - Add unit tests for sitemap.xml
 - Add unit tests for rss.xml
--- routes/tags/index.tsx
+++ routes/tags/index.tsx
@@ -1,17 +1,20 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { Link } from "../../components/Link.tsx";
 import { PageHeader } from "../../components/PageHeader.tsx";
-import { getAllTags } from "../../src/content.ts";
+import { getAllTags, getPostsByTag } from "../../src/content.ts";
 import { ServerState } from "../_middleware.ts";
 
+interface TagWithCount extends Tag {
+  count: number;
+}
 interface TagsProps {
-  tags: Tag[];
+  tags: TagWithCount[];
 }
 
 export const handler: Handlers<TagsProps, ServerState> = {
   async GET(req, ctx) {
     const url = new URL(req.url);
-    const tags = await getAllTags();
+    const tagsWithoutCount = await getAllTags();
     ctx.state.title = `Tags - ${ctx.state.title}`;
     ctx.state.breadcrumbs = [
       {
@@ -25,6 +28,15 @@ export const handler: Handlers<TagsProps, ServerState> = {
       },
     ];
 
+    const tags: TagWithCount[] = await Promise.all(
+      tagsWithoutCount.map(async (tag) => {
+        return {
+          ...tag,
+          count: await getTagPostCount(tag.slug),
+        };
+      }),
+    );
+
     return ctx.render({ ...ctx.state, tags });
   },
 };
@@ -37,10 +49,19 @@ export default function TagsIndex({ data }: PageProps<TagsProps>) {
       <ul class="columns-2 list-disc pl-4">
         {tags.map((tag) => (
           <li class="">
-            <Link href={tag.path} label={tag.title} />
+            <Link href={tag.path} label={`${tag.title} (${tag.count})`} />
           </li>
         ))}
       </ul>
     </article>
   );
+}
+
+async function getTagPostCount(tagSlug: string): Promise<number> {
+  const posts = await getPostsByTag(tagSlug);
+  if (!posts) {
+    return 0;
+  }
+
+  return posts.length;
 }
