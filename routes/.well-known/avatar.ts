8ae4aba5b167a1962296d60218fd4ff68920c09e
Tim Hårek Andreassen <tim@harek.no>
2023-08-25 21:50:18 +0200

    feat(well-known/avatar): Add new query params
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- routes/.well-known/avatar.ts
+++ routes/.well-known/avatar.ts
@@ -1,13 +1,35 @@
 import { Handlers } from "$fresh/server.ts";
+import { Image } from "https://deno.land/x/imagescript@1.2.15/mod.ts";
+
+type ImageType = "jpg" | "png";
 
 export const handler: Handlers = {
-  async GET(_req, _ctx) {
-    const avatarPath = new URL("../../static/img/me.jpeg", import.meta.url);
+  OPTIONS(_req, _ctx) {
+    const headers = new Headers();
+    headers.set("Allow", "GET, OPTIONS");
+    headers.set("Params", "type: 'jpg' | 'png', size: number, quality: number");
+    return new Response(null, { headers });
+  },
+  async GET(req, _ctx) {
+    const avatarPath = new URL("../../static/img/avatar.jpg", import.meta.url);
+    const params = new URL(req.url).searchParams;
+    const type = params.get("type") as ImageType | null;
+    const size = params.get("size") as number | null;
+    const quality = params.get("quality") as number | null;
     try {
-      const avatar = await Deno.readFile(avatarPath);
+      const DEFAULT_IMAGE_TYPE = "jpg";
+      const DEFAULT_IMAGE_SIZE = 200;
+      const DEFAULT_IMAGE_QUALITY = 100;
+      const avatarUncompressed = await Deno.readFile(avatarPath);
+      const avatar = await compressImage({
+        uncompressedImage: avatarUncompressed,
+        type: type ?? DEFAULT_IMAGE_TYPE,
+        width: size ? size : DEFAULT_IMAGE_SIZE,
+        quality: quality ? quality : DEFAULT_IMAGE_QUALITY,
+      });
 
       return new Response(avatar, {
-        headers: { "content-type": "image/jpeg" },
+        headers: { "content-type": `image/${type ?? DEFAULT_IMAGE_TYPE}` },
       });
     } catch (error) {
       console.error(error);
@@ -20,3 +42,30 @@ export const handler: Handlers = {
     }
   },
 };
+
+interface CompressProps {
+  uncompressedImage: Uint8Array;
+  type: ImageType;
+  width: number;
+  quality: number;
+}
+
+async function compressImage(
+  { uncompressedImage, width, type, quality }: CompressProps,
+) {
+  const image = await Image.decode(uncompressedImage);
+  const imageResized = image.resize(width, Image.RESIZE_AUTO);
+
+  const tmpPath = await Deno.makeTempFile({ suffix: `.${type}` });
+  if (type === "jpg") {
+    await Deno.writeFile(
+      tmpPath,
+      await imageResized.encodeJPEG(quality),
+    );
+  } else if (type === "png") {
+    await Deno.writeFile(tmpPath, await imageResized.encode(3));
+  }
+
+  const tmpFile = await Deno.readFile(tmpPath);
+  return tmpFile;
+}
