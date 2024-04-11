ff8c99f16c21bf81810563a94be4c09d7acbf3d2
Tim Hårek Andreassen <tim@harek.no>
2024-04-11 18:05:17 +0200

    refactor: Use `import.meta.main`
    Also clean up types
    
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- src/log/cli.ts
+++ src/log/cli.ts
@@ -4,9 +4,10 @@ import { selectKeys } from "../utils.ts";
 import { log } from "./index.ts";
 import { z } from "zod";
 
-type TypeSelector = {
-  [key: string]: (type: Entry["type"]) => Promise<Entry>;
-};
+type TypeSelector = Record<
+  Entry["type"],
+  (type: Entry["type"]) => Promise<Entry>
+>;
 
 const typeSelector: TypeSelector = {
   movie: log.movieTv,
@@ -14,26 +15,11 @@ const typeSelector: TypeSelector = {
   game: log.game,
   book: log.book,
   travel: log.trip,
+  life: log.life,
 };
 
-const type = await Select.prompt<Entry["type"]>({
-  message: "What do you want to log?",
-  options: [
-    { name: "Movie", value: "movie" },
-    { name: "TV Show", value: "tv" },
-    { name: "Game", value: "game" },
-    { name: "Book", value: "book" },
-    { name: "Travel", value: "travel" },
-    { name: "Life event", value: "life" },
-  ],
-  search: true,
-  keys: selectKeys,
-}) as Entry["type"];
-
-const newEntry = await typeSelector[type](type);
-
 const commonPath = "./static/api";
-export const logPath: Record<string, string> = {
+export const logPath: Record<Entry["type"], string> = {
   movie: `${commonPath}/movies.json`,
   tv: `${commonPath}/tv_shows.json`,
   game: `${commonPath}/games.json`,
@@ -42,12 +28,29 @@ export const logPath: Record<string, string> = {
   life: `${commonPath}/life.json`,
 };
 
-writeNewEntryToFile(logPath[type], newEntry);
-
 async function writeNewEntryToFile(path: string, entry: Entry) {
   const json = JSON.parse(await Deno.readTextFile(path));
   const allEntries = z.array(Log.Entry).parse(json);
   allEntries.push(entry);
   console.log(entry);
   await Deno.writeTextFile(path, JSON.stringify(allEntries, null, 2));
+}
+
+if (import.meta.main) {
+  const type = await Select.prompt<Entry["type"]>({
+    message: "What do you want to log?",
+    options: [
+      { name: "Movie", value: "movie" },
+      { name: "TV Show", value: "tv" },
+      { name: "Game", value: "game" },
+      { name: "Book", value: "book" },
+      { name: "Travel", value: "travel" },
+      { name: "Life event", value: "life" },
+    ],
+    search: true,
+    keys: selectKeys,
+  }) as Entry["type"];
+
+  const newEntry = await typeSelector[type](type);
+  writeNewEntryToFile(logPath[type], newEntry);
 }
