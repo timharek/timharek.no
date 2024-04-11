f9b941a57e1e51fcee8757d22105a6c94d2af9f6
Tim Hårek Andreassen <tim@harek.no>
2024-04-11 22:07:57 +0200

    style: Use monofont for deploy date
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,7 @@ export function Footer({ currentPath, lastDeploy }: FooterProps) {
   return (
     <footer class="print:hidden max-w-screen-md mx-auto my-4 px-4 flex justify-between flex-wrap">
       <div>
-        Last deploy: {lastDeploy}
+        Last deploy: <span className="font-mono">{lastDeploy}</span>
       </div>
 
       <nav class="footer__nav" aria-label="Secondary navigation">
