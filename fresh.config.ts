3168d0b3682cc15acb80c867f057241df1b90772
Tim Hårek Andreassen <tim@harek.no>
2024-04-12 08:49:45 +0200

    chore: Make fresh config available to `main.ts` and `dev.ts
    References: https://github.com/denoland/fresh/issues/1678
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

---
+++ fresh.config.ts
@@ -0,0 +1,15 @@
+import "$std/dotenv/load.ts";
+import { defineConfig } from "$fresh/server.ts";
+import twindPlugin from "$fresh/plugins/twindv1.ts";
+import twindConfig from "./twind.config.ts";
+
+Deno.env.set("HOSTNAME", Deno.env.get("HOSTNAME") ?? "localhost");
+Deno.env.set("PORT", Deno.env.get("PORT") ?? "8000");
+export const hostname = Deno.env.get("HOSTNAME");
+export const port = Deno.env.get("PORT");
+
+export default defineConfig({
+  hostname,
+  port: Number(port),
+  plugins: [twindPlugin(twindConfig)],
+});
--- main.ts
+++ main.ts
@@ -1,25 +1,5 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="dom" />
-/// <reference lib="dom.iterable" />
-/// <reference lib="dom.asynciterable" />
-/// <reference lib="deno.ns" />
-
-import "$std/dotenv/load.ts";
-
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
 
-import twindPlugin from "$fresh/plugins/twindv1.ts";
-import twindConfig from "./twind.config.ts";
-
-Deno.env.set("HOSTNAME", Deno.env.get("HOSTNAME") ?? "localhost");
-Deno.env.set("PORT", Deno.env.get("PORT") ?? "8000");
-
-export const hostname = Deno.env.get("HOSTNAME");
-export const port = Deno.env.get("PORT");
-
-await start(manifest, {
-  hostname,
-  port: Number(port),
-  plugins: [twindPlugin(twindConfig)],
-});
+await start(manifest, config);
