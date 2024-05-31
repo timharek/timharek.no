e51dbb2fa866eda4bcfe0cfc67c79322e20bccc5
Tim Hårek Andreassen <tim@harek.no>
2024-05-31 15:17:08 +0200

    fix: Use proper Atom schema
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- routes/feed.xml/index.ts
+++ routes/feed.xml/index.ts
@@ -45,10 +45,6 @@ function generateXML(posts: Post[]): string {
   });
   const feedUrl = `${config.base_url}/feed.xml`;
   const feedXML = stringify({
-    xml: {
-      "@version": "1.0",
-      "@encoding": "UTF-8",
-    },
     feed: {
       "@xmlns": "http://www.w3.org/2005/Atom",
       "@xml:lang": "en",
